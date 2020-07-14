8cb6328f74d3671c6ab645404bc7e6513de87dd9
Zain Ali <z.a.cheeema7777@gmail.com>
2020-07-14 17:51:21 +0500

    added nav_graph, designed destination fragment layout

---
+++ app/src/main/java/com/example/android/lcohomeworkout/WorkoutFinishedFragment.java
@@ -0,0 +1,26 @@
+package com.example.android.lcohomeworkout;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutFinishedFragment extends Fragment {
+
+    public WorkoutFinishedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_finished, container, false);
+    }
+}
