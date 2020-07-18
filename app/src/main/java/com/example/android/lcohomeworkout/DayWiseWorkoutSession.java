1d208cc97b14013cc5ecc48d5bad369a3082b0a5
Zain Ali <z.a.cheeema7777@gmail.com>
2020-07-18 13:28:27 +0500

    Update DayWiseWorkoutSession.java

--- app/src/main/java/com/example/android/lcohomeworkout/DayWiseWorkoutSession.java
+++ app/src/main/java/com/example/android/lcohomeworkout/DayWiseWorkoutSession.java
@@ -1,89 +1,120 @@
 package com.example.android.lcohomeworkout;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class DayWiseWorkoutSession {
-    private static List<Workout> MONDAY_SESSION;
-    private static List<Workout> TUESDAY_SESSION;
-    private static List<Workout> WEDNESDAY_SESSION;
-    private static List<Workout> THURSDAY_SESSION;
-    private static List<Workout> FRIDAY_SESSION;
-    private static List<Workout> SATURDAY_SESSION;
-    private static List<Workout> SUNDAY_SESSION;
+    private static List<Workout> mondaySession;
+    private static List<Workout> tuesdaySession;
+    private static List<Workout> wednesdaySession;
+    private static List<Workout> thursdaySession;
+    private static List<Workout> fridaySession;
+    private static List<Workout> saturdaySession;
+    private static List<Workout> sundaySession;
+
+    public static List<Workout> getTodaySession() {
+        List<Workout> todaySession;
+        switch (Calendar.DAY_OF_WEEK) {
+            case Calendar.MONDAY:
+                todaySession = getMondaySession();
+                break;
+            case Calendar.TUESDAY:
+                todaySession = getTuesdaySession();
+                break;
+            case Calendar.WEDNESDAY:
+                todaySession = getWednesdaySession();
+                break;
+            case Calendar.THURSDAY:
+                todaySession = getThursdaySession();
+                break;
+            case Calendar.FRIDAY:
+                todaySession = getFridaySession();
+                break;
+            case Calendar.SATURDAY:
+                todaySession = getSaturdaySession();
+                break;
+            case Calendar.SUNDAY:
+                todaySession = getSundaySession();
+                break;
+            default:
+                todaySession = null;
+        }
+        return todaySession;
+    }
 
     public static List<Workout> getMondaySession() {
-        if (MONDAY_SESSION == null) {
-            MONDAY_SESSION = new Workout.SessionBuilder(3)
+        if (mondaySession == null) {
+            mondaySession = new Workout.SessionBuilder(3)
                     .addBenchPressOfSets(3)
                     .addShoulderPressOfSets(3)
                     .addPushUpsOfSets(3)
                     .build();
         }
-        return MONDAY_SESSION;
+        return mondaySession;
     }
 
     public static List<Workout> getTuesdaySession() {
-        if (TUESDAY_SESSION == null) {
-            TUESDAY_SESSION = new Workout.SessionBuilder(2)
+        if (tuesdaySession == null) {
+            tuesdaySession = new Workout.SessionBuilder(2)
                     .addPlankOfSets(3)
                     .addCrunchesOfSets(2)
                     .addSLegRaiseOfSets(2)
                     .build();
         }
-        return TUESDAY_SESSION;
+        return tuesdaySession;
     }
 
     public static List<Workout> getWednesdaySession() {
-        if (WEDNESDAY_SESSION == null) {
-            WEDNESDAY_SESSION = new Workout.SessionBuilder(3)
+        if (wednesdaySession == null) {
+            wednesdaySession = new Workout.SessionBuilder(3)
                     .addPlankOfSets(2)
                     .addSquatOfSets(3)
                     .addLungesOfSets(3)
                     .addShoulderPressOfSets(3)
                     .build();
         }
-        return WEDNESDAY_SESSION;
+        return wednesdaySession;
     }
 
     public static List<Workout> getThursdaySession() {
-        if (THURSDAY_SESSION == null) {
-            THURSDAY_SESSION = new Workout.SessionBuilder(3)
+        if (thursdaySession == null) {
+            thursdaySession = new Workout.SessionBuilder(3)
                     .addPushUpsOfSets(3)
                     .addBarbellCurlOfSets(3)
                     .build();
         }
-        return THURSDAY_SESSION;
+        return thursdaySession;
     }
 
     public static List<Workout> getFridaySession() {
-        if (FRIDAY_SESSION == null) {
-            FRIDAY_SESSION = new Workout.SessionBuilder(3)
+        if (fridaySession == null) {
+            fridaySession = new Workout.SessionBuilder(3)
                     .addBenchPressOfSets(3)
                     .addPushUpsOfSets(3)
                     .addLungesOfSets(3)
                     .build();
         }
-        return FRIDAY_SESSION;
+        return fridaySession;
     }
 
     public static List<Workout> getSaturdaySession() {
-        if (SATURDAY_SESSION == null) {
-            SATURDAY_SESSION = new Workout.SessionBuilder(2)
+        if (saturdaySession == null) {
+            saturdaySession = new Workout.SessionBuilder(2)
                     .addShoulderPressOfSets(2)
                     .addSquatOfSets(2)
                     .addBarbellCurlOfSets(2)
                     .build();
         }
-        return SATURDAY_SESSION;
+        return saturdaySession;
     }
 
     public static List<Workout> getSundaySession() {
-        if (SUNDAY_SESSION == null) {
-            SUNDAY_SESSION = new Workout.SessionBuilder(3)
+        if (sundaySession == null) {
+            sundaySession = new Workout.SessionBuilder(3)
                     .addCrunchesOfSets(3)
                     .addSLegRaiseOfSets(3)
                     .build();
         }
-        return SUNDAY_SESSION;
+        return sundaySession;
     }
 }
