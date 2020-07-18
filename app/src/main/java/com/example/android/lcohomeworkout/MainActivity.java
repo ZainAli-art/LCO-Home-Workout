8db1bad94f3736c32335c146e6f653d899790d2f
Zain Ali <z.a.cheeema7777@gmail.com>
2020-07-18 15:18:41 +0500

    Created CreatorFragment and added it to the Navigation Graph

--- app/src/main/java/com/example/android/lcohomeworkout/MainActivity.java
+++ app/src/main/java/com/example/android/lcohomeworkout/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.android.lcohomeworkout;
 
+import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MenuItem.OnMenuItemClickListener {
+    private Toolbar toolbar;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navController = Navigation.findNavController(this, R.id.navFragment);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(this::onMenuItemClick);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, navController);
     }
 }
